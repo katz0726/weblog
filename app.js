6c0f12aa8e0bb301004130cdf93951c9f378de3d
shoya <swgt1500@gmail.com>
2020-09-23 04:36:55 +0900

    静的部分の共通機能を実装

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-console.log("Hello, world");
+var express = require("express");
+var app = express();
 
-console.log("Hello, world");
+app.set("view engine", "ejs");
+app.disable("x-powered-by");
+
+app.use("/public", express.static(__dirname + "/public/" + (process.env.NODE_ENV === "development" ? "development" : "production")));
+
+app.use("/", require("./routes/index.js"));
+
+app.listen(3000);
